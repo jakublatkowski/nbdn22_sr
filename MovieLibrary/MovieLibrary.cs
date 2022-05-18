21d4be5a6f1d9af15923a8ab700a11f187ba84c8
manuxx <manusxyz@gmail.com>
2022-05-18 11:10:06 +0200

    rozwiazanie + wyrefaktoryzowana MoviesThatSatisfy

--- MovieLibrary/MovieLibrary.cs
+++ MovieLibrary/MovieLibrary.cs
@@ -26,7 +26,12 @@ namespace TrainingPrep.collections
             }
             movies.Add(movie);
         }
-
+        public IEnumerable<Movie> sort_all_movies_by_title_descending()
+        {
+            var ret = new List<Movie>(movies);
+            ret.Sort((m1, m2) => (-1) * m1.title.CompareTo(m2.title));
+            return ret;
+        }
         public IEnumerable<Movie> all_movies_published_by_pixar()
         {
             foreach (var movie in movies)
@@ -38,11 +43,55 @@ namespace TrainingPrep.collections
             }
         }
 
-        public IEnumerable<Movie> sort_all_movies_by_title_descending()
+        public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
         {
-            var ret = new List<Movie>(movies);
-            ret.Sort((m1,m2)=> (-1)*m1.title.CompareTo(m2.title));
-            return ret;
+            return MovieThatSatisfy(movie => movie.production_studio == ProductionStudio.Pixar || movie.production_studio == ProductionStudio.Disney);
+        }
+
+        private IEnumerable<Movie> MovieThatSatisfy(Func<Movie, bool> condition)
+        {
+            foreach (var movie in movies)
+            {
+                if (condition(movie))
+                {
+                    yield return movie;
+                }
+            }
+        }
+
+        public IEnumerable<Movie> all_movies_not_published_by_pixar()
+        {
+            return MovieThatSatisfy(m => m.production_studio != ProductionStudio.Pixar);
+        }
+
+        public IEnumerable<Movie> all_movies_published_after(int year)
+        {
+            return MovieThatSatisfy(m => m.date_published.Year > year);
+        }
+
+        public IEnumerable<Movie> all_movies_published_between_years(int startYear, int endYear)
+        {
+            return MovieThatSatisfy(m => m.date_published.Year >= startYear && m.date_published.Year <= endYear);
+        }
+
+        public IEnumerable<Movie> all_kid_movies()
+        {
+            return MovieThatSatisfy(m => m.genre == Genre.kids);
+        }
+
+        public IEnumerable<Movie> all_action_movies()
+        {
+            return MovieThatSatisfy(m => m.genre == Genre.action);
+        }
+
+        public IEnumerable<Movie> all_kid_movies_published_after(int year)
+        {
+            return MovieThatSatisfy(m => m.genre == Genre.kids && m.date_published.Year > year);
+        }
+
+        public IEnumerable<Movie> all_horror_or_action()
+        {
+            return MovieThatSatisfy(m => m.genre == Genre.action || m.genre == Genre.horror);
         }
     }
 }
